7e6ca6e66479084cb36dcba6a2b543f56e188e5b
Student <letlaurens@email.com>
2020-07-29 17:51:46 -0300

    Exercício 29/07

---
+++ CRUD/esolaJasmin/src/main/java/com/minha/esolaJasmin/model/Turma.java
@@ -0,0 +1,42 @@
+package com.minha.esolaJasmin.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table (name = "tb_classe")
+public class Turma {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size (min = 5, max = 50)
+	private String turma;
+	private boolean ativo;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTurma() {
+		return turma;
+	}
+	public void setTurma(String turma) {
+		this.turma = turma;
+	}
+	public boolean isAtivo() {
+		return ativo;
+	}
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+}
---
+++ CRUD/esolaJasmin/src/test/java/com/minha/esolaJasmin/EsolaJasminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minha.esolaJasmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsolaJasminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
